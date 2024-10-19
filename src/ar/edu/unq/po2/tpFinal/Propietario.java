5a27df1fa9f946029158c2f4373ec0b65783a603
valentinachiappori <valchiappori@gmail.com>
2024-10-19 18:19:17 -0300

    prueba

--- src/ar/edu/unq/po2/tpFinal/Propietario.java
+++ src/ar/edu/unq/po2/tpFinal/Propietario.java
@@ -1,15 +1,5 @@
 package ar.edu.unq.po2.tpFinal;
 
 public class Propietario {
-	private String apellido;
-
-	public String getApellido() {
-		return apellido;
-	}
-
-	public void setApellido(String apellido) {
-		this.apellido = apellido;
-	}
-	
 	
 }
