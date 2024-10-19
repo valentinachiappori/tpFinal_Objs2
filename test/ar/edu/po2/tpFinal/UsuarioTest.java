4966833778b0e1d5fc695fb279f1c1d6274006da
valentinachiappori <valchiappori@gmail.com>
2024-10-19 17:51:34 -0300

    probando

---
+++ test/ar/edu/po2/tpFinal/UsuarioTest.java
@@ -0,0 +1,19 @@
+package ar.edu.po2.tpFinal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class UsuarioTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
