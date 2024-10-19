061b7e47dbf7dda99b2120259e1339ca814f9cb9
nicotorboli <nicotorboli1@gmail.com>
2024-10-19 18:16:48 -0300

    probando

--- src/ar/edu/unq/po2/tpFinal/Usuario.java
+++ src/ar/edu/unq/po2/tpFinal/Usuario.java
@@ -2,4 +2,13 @@ package ar.edu.unq.po2.tpFinal;
 
 public class Usuario {
 	private String nombreCompleto;
+
+	public String getNombreCompleto() {
+		return nombreCompleto;
+	}
+
+	public void setNombreCompleto(String nombreCompleto) {
+		this.nombreCompleto = nombreCompleto;
+	}
+	
 }
