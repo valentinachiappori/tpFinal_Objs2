0a161f241c8416dce8aa8ce777e1a3669730c749
nicotorboli <nicotorboli1@gmail.com>
2024-10-29 16:56:47 -0300

    agregado de getters y setter mas colaborador interno inmuebles

--- src/ar/edu/unq/po2/tpFinal/Propietario.java
+++ src/ar/edu/unq/po2/tpFinal/Propietario.java
@@ -1,11 +1,33 @@
 package ar.edu.unq.po2.tpFinal;
 
-public class Propietario extends Usuario {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Propietario extends Usuario {
+	
+	private List<Inmueble> inmuebles;
+	
 	public Propietario(String nombreCompleto, int numeroDeTelefono, String correoElectronico) {
 		super(nombreCompleto, numeroDeTelefono, correoElectronico);
 		// TODO Auto-generated constructor stub
+		setInmuebles(new ArrayList<Inmueble>());
 	}
 	
+	public void ponerEnAlquilerUnImbueble(Inmueble i) {
+		
+	}
+
+	public List<Inmueble> getInmuebles() {
+		return inmuebles;
+	}
+
+	public void setInmuebles(List<Inmueble> inmuebles) {
+		this.inmuebles = inmuebles;
+	}
+	
+    public void agregarInmueble(Inmueble i) {
+        inmuebles.add(i);
+    }
+
 	
 }
