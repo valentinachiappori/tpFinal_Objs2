9f0cc70dcb68a4431665d4d8c5e43e3eb21302d6
valentinachiappori <valchiappori@gmail.com>
2024-10-31 15:08:16 -0300

    cambio en filtrarInmuebles

--- src/ar/edu/unq/po2/tpFinal/SitioWeb.java
+++ src/ar/edu/unq/po2/tpFinal/SitioWeb.java
@@ -19,7 +19,7 @@ public class SitioWeb {
 		List<Inmueble> inmueblesFiltrados = inmuebles.stream()
 		.filter(i -> i.getCiudad().equals(ciudad))
 		.filter(i -> i.estaDisponibleEnPeriodo(fechaEntrada, fechaSalida))
-		.collect(Collectors.toList());
+		.toList();
 		//nos faltan los nullsssssssss aaa
 		return inmueblesFiltrados;
 	}
