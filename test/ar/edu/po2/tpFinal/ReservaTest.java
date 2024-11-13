e456f20c9f389cd84d3f21f854054390b31da7c5
lucrecolon <lucrecolon1828@gmail.com>
2024-11-12 22:56:57 -0300

    nuevo test reserva

--- test/ar/edu/po2/tpFinal/ReservaTest.java
+++ test/ar/edu/po2/tpFinal/ReservaTest.java
@@ -2,23 +2,54 @@ package ar.edu.po2.tpFinal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.AfterEach;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import ar.edu.unq.po2.tpFinal.Inmueble;
+import ar.edu.unq.po2.tpFinal.Reserva;
+import ar.edu.unq.po2.tpFinal.Usuario;
+
 
 class ReservaTest {
 
+	private Reserva reserva;
+	private LocalDate fechaEntrada;
+	private LocalDate fechaSalida;
+	private Inmueble inmueble;
+	private Usuario inquilino;
+	private String estadoReserva;
+	
+	private Usuario propietario;
+	
+	
 	@BeforeEach
 	void setUp() throws Exception {
+		inmueble = mock(Inmueble.class);
+		inquilino = mock(Usuario.class);
+		fechaEntrada = LocalDate.of(2024, 9, 11);
+		fechaSalida = LocalDate.of(2024, 12, 11);
+		reserva = new Reserva(fechaEntrada, fechaSalida, inmueble, inquilino);
+		propietario = mock(Usuario.class);
 	}
 
-	@AfterEach
-	void tearDown() throws Exception {
-	}
-
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testGetPropietario() {
+		when(inmueble.getPropietario()).thenReturn(propietario);
+		
+		assertEquals(reserva.getPropietario(), propietario);
+		
 	}
 
 }
