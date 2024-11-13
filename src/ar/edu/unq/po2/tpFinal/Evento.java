296dcaa4c7b774bbc70483157f606d1569c1ecb3
nicotorboli <nicotorboli1@gmail.com>
2024-11-13 03:29:47 -0300

    se actualizo el mensaje de cancelacion de evento

--- src/ar/edu/unq/po2/tpFinal/Evento.java
+++ src/ar/edu/unq/po2/tpFinal/Evento.java
@@ -4,23 +4,22 @@ import java.time.LocalDate;
 import java.util.function.Function;
 
 public enum Evento {
-	  CANCELACION(inmueble -> "un inmueble " + inmueble.getTipoInmueble() + "a tan sólo $" + inmueble.calcularPrecioDia(LocalDate.now())),
-	  RESERVA(inmueble -> "un inmueble " + inmueble.getTipoInmueble() + "a tan sólo $" + inmueble.calcularPrecioDia(LocalDate.now())),
-	  BAJAPRECIO(inmueble -> "El/la " + inmueble.getTipoInmueble() + "que te interesa se ha liberado! Corre a reservarlo!");
+    CANCELACION(inmueble -> "un inmueble " + inmueble.getTipoInmueble() + " a tan sólo $" + inmueble.calcularPrecioDia(LocalDate.now())),
+    RESERVA(inmueble -> "un inmueble " + inmueble.getTipoInmueble() + " el inmueble fue reservado"),
+    BAJAPRECIO(inmueble -> "El/la " + inmueble.getTipoInmueble() + " que te interesa se ha liberado! Corre a reservarlo!");
 
+    private Function<Inmueble, String> mensaje;
 
-	private Function<Inmueble, String> mensaje;
-	
+    Evento(Function<Inmueble, String> mensaje) {
+        setMensaje(mensaje);
+    }
 
-	Evento(Function<Inmueble, String> mensaje) {
-		setMensaje(mensaje);
-};
-    
     public String aplicarMensaje(Inmueble inmueble) {
         return mensaje.apply(inmueble);
     }
-    
+
     private void setMensaje(Function<Inmueble, String> mensaje) {
-    	this.mensaje = mensaje;
+        this.mensaje = mensaje;
     }
 }
+
